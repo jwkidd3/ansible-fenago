7dcfee429928d23d40471325e975d43569b0d893
jwkidd3 <jkidd@kiddcorp.com>
2022-12-06 20:20:41 -0600

    Update ec2_instances.tf

--- terraform/ec2_instances.tf
+++ terraform/ec2_instances.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
